0d30a55178e8d643b5d46bdaa812f8618865cce9
Philip Yu <158358432+Phboy1@users.noreply.github.com>
2026-06-01 18:46:24 -0400

    Refactor Ghost class to handle death and animation

--- Ghost.java
+++ Ghost.java
@@ -1,9 +1,29 @@
 package TileScroller;
 
 import java.awt.*;
+import java.io.File;
 import java.util.ArrayList;
 
+import javax.imageio.ImageIO;
+
 public class Ghost {
+    Image sprite;
+
+    int frameAmount = 6;
+    int frame = 0;
+    long frameLength = 75000000L;
+    long lastFrame = 0;
+    boolean wasMoving;
+    String lastDirection;
+
+    long currentTime;
+
+    int deathFrame = 0;
+    int deathFrameAmount = 4;
+    long deathFrameLength = 150000000L;
+    long lastDeathFrame = 0;
+    Image[] deathSprites = new Image[4];
+
     int ghostX;
     int ghostY;
     int ghostCameraX;
@@ -12,7 +32,7 @@ public class Ghost {
     int ghostIndex;
     int i = 0;
     boolean finished = false;
-    boolean isDead;
+    boolean isDead = false;
 
     static final Color DEAD_COLOR = Color.DARK_GRAY;
     static final Color ALIVE_COLOR = Color.CYAN;
@@ -21,6 +41,25 @@ public class Ghost {
     {
         this.ghostIndex = ghostIndex;
         this.size = size;
+        
+        try{
+            sprite = ImageIO.read(new File("TileScroller/assets/ghostIdleDown.png"));
+        } catch(Exception e)
+        {
+            System.out.println("WRONG");
+        }
+
+        try
+        {
+            for (int i = 1; i <= deathFrameAmount; i++)
+            {
+                deathSprites[i-1] = ImageIO.read(new File("TileScroller/assets/ghostDeath" + i + ".png"));
+            } 
+        } 
+        catch(Exception e)
+        {
+            System.out.println("LOADING GHOST TOO HARD");
+        }
     }
 
     public void update()
@@ -36,6 +75,66 @@ public class Ghost {
             ghostCameraX -= actions.get(i).cameraX;
             ghostCameraY -= actions.get(i).cameraY;
 
+            if (isDead)
+            {
+                currentTime = System.nanoTime();
+                if (currentTime - lastDeathFrame > deathFrameLength)
+                {
+                    if (deathFrame >= deathFrameAmount)
+                    {
+                        finished = true; 
+                    }
+                    else
+                    {
+                        deathFrame++;
+                        lastDeathFrame = currentTime;
+                    }
+                    System.out.println(deathFrame);
+                }                
+
+                sprite = deathSprites[deathFrame-1];
+            }
+            else if (!Culminating.rewinding)
+            {
+                deathFrame = 0;
+                boolean moving = false;
+
+                if (actions.get(i).playerX != 0 || actions.get(i).playerY != 0 || actions.get(i).cameraX != 0 || actions.get(i).cameraY != 0) moving = true;
+
+                
+                try 
+                {
+                    if (moving)
+                    {
+                        currentTime = System.nanoTime();
+
+                        if (currentTime - lastFrame > frameLength)
+                        {
+                            frame = (frame + 1) % frameAmount;
+                            lastFrame = currentTime;
+                        }
+
+                        if (actions.get(i).cameraY > 0 || actions.get(i).playerY > 0) sprite = ImageIO.read(new File("TileScroller/assets/ghostWalkDown" + frame + ".png"));
+                        if (actions.get(i).cameraY < 0 || actions.get(i).playerY < 0) sprite = ImageIO.read(new File("TileScroller/assets/ghostWalkUp" + frame + ".png"));
+                        if (actions.get(i).cameraX > 0 || actions.get(i).playerX > 0) sprite = ImageIO.read(new File("TileScroller/assets/ghostWalkRight" + frame + ".png"));
+                        if (actions.get(i).cameraX < 0 || actions.get(i).playerX < 0) sprite = ImageIO.read(new File("TileScroller/assets/ghostWalkLeft" + frame + ".png"));
+
+                        wasMoving = true;
+                        lastDirection = ((actions.get(i).cameraY > 0 || actions.get(i).playerY > 0) ? "Down" : ((actions.get(i).cameraY < 0 || actions.get(i).playerY < 0) ? "Up" : ((actions.get(i).cameraX > 0 || actions.get(i).playerX > 0) ? "Right" : "Left")));
+                        }
+                    else if (wasMoving)
+                    {
+                        sprite = ImageIO.read(new File("TileScroller/assets/ghostIdle" + lastDirection + ".png"));
+                    }
+                } catch (Exception e) {
+                    System.out.println("GHOST TOO HARD");
+                }
+            }
+            
+
+            
+
+
             if (actions.get(i).interacted)
             {
                 System.out.println("GHOST INTERACTING: " + actions.get(i).interactedItemId);
@@ -44,8 +143,7 @@ public class Ghost {
                     if (item.id.equals(actions.get(i).interactedItemId))
                     {
                         //System.out.println("SETTING " + item.id + " TO TRUE");
-                        item.activated = true;
-                        
+                        item.activated = true;          
                     }
                 }
             }
@@ -71,7 +169,9 @@ public class Ghost {
         }
         int drawX = WIDTH / 2 - size / 2 - ghostX + (currentCameraX - ghostCameraX);
         int drawY = HEIGHT / 2 - size / 2 - ghostY + (currentCameraY - ghostCameraY);
-        g2d.fillRect(drawX, drawY, size, size);
+        //g2d.fillRect(drawX, drawY, size, size);
+        g2d.drawImage(sprite, drawX, drawY, size, size, null);
+
 
         g2d.setColor(Color.WHITE);
         g2d.setFont(new Font("Serif", Font.BOLD, 30));
